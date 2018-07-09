b97134105726a642eaaa53d670ad01ea0f719bd3
camilasousa <miyuki.sousa@gmail.com>
2018-07-09 19:43:41 -0300

    feature: Add react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers';
+import App from './App';
+import './index.css';
+
+
+const store = createStore(
+  reducer,
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
