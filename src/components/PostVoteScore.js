77e96181fca510251403ba00963fee8fdcc85c69
camilasousa <miyuki.sousa@gmail.com>
2018-08-22 22:19:45 -0300

    refactor: remove unused mapStateToProps on PostVoteScore

--- src/components/PostVoteScore.js
+++ src/components/PostVoteScore.js
@@ -3,14 +3,12 @@ import { connect } from 'react-redux';
 import VoteScore from './VoteScore';
 import { downVotePost, upVotePost } from '../actions/posts';
 
-const mapStateToProps = () => ({});
-
 const mapDispatchToProps = dispatch => ({
   downVote: id => dispatch(downVotePost(id)),
   upVote: id => dispatch(upVotePost(id)),
 });
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps,
 )(VoteScore);
